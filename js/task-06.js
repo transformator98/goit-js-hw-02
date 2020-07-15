0f3985030b6eb68da862938b72246fe591ac361c
Ivan Polovynka <Vanushka16@gmail.com>
2020-07-15 18:53:19 +0300

    redact task6

--- js/task-06.js
+++ js/task-06.js
@@ -1,21 +1,46 @@
+// let input;
+// const numbers = [];
+// let total = 0;
+
+// do {
+//   input = prompt("Введите число!");
+//   numbers.push(input);
+//   console.log(numbers);
+
+//   if (input === null) {
+//     break;
+//   }
+// } while (true);
+// for (const number of numbers) {
+//   const i = Number(number);
+//   total += i;
+//   if (total === 0) {
+//     console.log(`Вы не ввели число!`);
+//   }
+// }
+// console.log(`Общая сумма чисел равна ${total}`);
+
 let input;
 const numbers = [];
 let total = 0;
 
 do {
   input = prompt("Введите число!");
-  numbers.push(input);
-  console.log(numbers);
 
   if (input === null) {
+    console.log;
     break;
   }
-} while (true);
-for (const number of numbers) {
-  const i = Number(number);
-  total += i;
-  if (total === 0) {
-    console.log(`Вы не ввели число!`);
+  input = Number(input);
+  numbers.push(input);
+} while (input !== null);
+{
+  for (const number of numbers) {
+    total += number;
+    if (total === 0) {
+      console.log(`Вы не ввели число!`);
+    }
   }
+  console.log(`Общая сумма чисел равна ${total}`);
+  console.log(numbers);
 }
-console.log(`Общая сумма чисел равна ${total}`);
